3887ca66ed92b1ada4fff0293dc0124b196b3207
Ehigie 'Pystar' Aito <aitoehigie@gmail.com>
2013-11-06 21:36:48 +0100

    Created a python wrapper for fonenode.com

---
+++ fonenode.py
@@ -0,0 +1,6 @@
+#Python wrapper for the Fonenode API (fonenode.com/docs)
+#
+# https://github.com/aitoehigie/python_fonenode
+#
+# @author Pascal Ehigie Aito (@pystar) <aitoehigie@gmail.com>
+# @version 0.0.1
